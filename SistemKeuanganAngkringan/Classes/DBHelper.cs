1a27023405b059da3b395b1cfec4e28ace0adb03
Wulandhika <wulandhikakurnaliawati@gmail.com>
2026-05-09 13:33:03 +0700

    CEK STATUS KONEKSI DBHELPER

--- SistemKeuanganAngkringan/Classes/DBHelper.cs
+++ SistemKeuanganAngkringan/Classes/DBHelper.cs
@@ -8,3 +8,34 @@ namespace SistemKeuanganAngkringan.Classes
         // Koneksi ke database DBshop (username 10, password 10)
         private static string connectionString = @"Data Source=LAPTOP-07AAA94J\SQLEXPRESS;Initial Catalog=DBshop;Integrated Security=True";
 
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        public static void OpenConnection(SqlConnection conn)
+        {
+            if (conn == null)
+                return;
+
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+        }
+
+        public static void CloseConnection(SqlConnection conn)
+        {
+            if (conn == null)
+                return;
+
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+        }
+
+        // Method untuk cek status koneksi
+        public static bool IsConnectionOpen(SqlConnection conn)
+        {
+            if (conn == null)
+                return false;
+            return conn.State == ConnectionState.Open;
+        }
+
